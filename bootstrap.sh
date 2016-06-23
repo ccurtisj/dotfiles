735b85396ec275c3eb7fc36f8bee512b9e9d441b
Christopher Curtis <ccurtisj@gmail.com>
2016-06-23 15:41:43 -0500

    Add initial configs

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+mkdir ~/bin
+
+ln -s "/Users/curtis/dotfiles/.bash_profile" ~/.bash_profile
+
+ln -s "/Users/curtis/dotfiles/.gitconfig" ~/.gitconfig
+
+ln -s "/Users/curtis/dotfiles/Preferences.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
