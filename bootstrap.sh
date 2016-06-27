5ada61d09cf4c2fc0848d72e2040ebc51dc57dbd
Christopher Curtis <ccurtisj@gmail.com>
2016-06-27 13:09:19 -0500

    Properly confugure sublime text to sync entire User folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,6 @@ ln -s "/Users/curtis/dotfiles/.bash_profile" ~/.bash_profile
 
 ln -s "/Users/curtis/dotfiles/.gitconfig" ~/.gitconfig
 
-ln -s "/Users/curtis/dotfiles/Preferences.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -s "/Users/curtis/dotfiles/SublimeText-Packages-User" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
