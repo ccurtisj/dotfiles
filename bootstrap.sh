da3362bf95fd5fb2384360470d365d12be05db0a
Christopher Curtis <ccurtisj@gmail.com>
2020-07-17 12:05:55 -0500

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,6 @@
 
 mkdir ~/bin
 
-ln -s "/Users/curtis/dotfiles/.bash_profile" ~/.bash_profile
+ln -s "/Users/ccurtisj/dotfiles/.bash_profile" ~/.bash_profile
 
-ln -s "/Users/curtis/dotfiles/.gitconfig" ~/.gitconfig
-
-ln -s "/Users/curtis/dotfiles/SublimeText-Packages-User" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-
-ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+ln -s "/Users/ccurtisj/dotfiles/.gitconfig" ~/.gitconfig
